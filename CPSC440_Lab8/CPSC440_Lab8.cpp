37a58f9f25c279b14c88880e84f958b6d25fb968
adantuon <adantuon@mail.umw.edu>
2025-06-04 14:36:38 -0400

    Added basic Allegro functionality.

--- CPSC440_Lab8/CPSC440_Lab8.cpp
+++ CPSC440_Lab8/CPSC440_Lab8.cpp
@@ -4,4 +4,56 @@
 
 int main() {
 
+	const int FPS = 60;
+	int width = 640;
+	int height = 480;
+	bool exit = false;
+
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *eventQueue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *background = NULL;
+	ALLEGRO_BITMAP *character = NULL;
+
+	//program init
+	if (!al_init())
+		return -1;
+
+	//display creation
+	display = al_create_display(width, height);
+
+	if (!display)
+		return -1;
+
+	//timer creation
+	timer = al_create_timer(1.0 / FPS);
+	if (!timer)
+		return -1;
+
+	//event queue creation
+	eventQueue = al_create_event_queue();
+	if (!eventQueue)
+		return -1;
+
+	//image init
+	if (!al_init_image_addon())
+		return -1;
+
+	//Load Images
+	character = al_load_bitmap("PixelArtCharacter.png");
+	background = al_load_bitmap("Grass.png");
+
+	//Register event sources
+	al_register_event_source(eventQueue, al_get_display_event_source(display));
+	al_register_event_source(eventQueue, al_get_timer_event_source(timer));
+
+	while (!exit) {
+		ALLEGRO_EVENT event;
+		al_wait_for_event(eventQueue, &event);
+
+		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			exit = true;
+		}
+	}
+
 }
